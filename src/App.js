4f6ca0ca9f45ad36bb742becc07572f84139c952
Menard Maranan <66345147+menard-codes@users.noreply.github.com>
2021-07-28 20:22:23 +0800

    some finishing aspects

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react';
 import Select from 'react-select';
 import axios from 'axios';
 import { currencyCodes } from './currencyLabel';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExchangeAlt, faCommentsDollar } from '@fortawesome/free-solid-svg-icons'
 import 'dotenv/config';
 
 import './App.css';
@@ -13,6 +15,7 @@ export default function App() {
   const [pair, setPair] = useState(currencyCodes[43]); // EUR
   const [baseAmount, setBaseAmount] = useState(1);
   const [conversion, setConversion] = useState('');
+  const [width, setInnerWidth] = useState(window.innerWidth)
 
   useEffect(() => {
     const url = `https://v6.exchangerate-api.com/v6/${process.env.REACT_APP_API_KEY}/pair/${base.value}/${pair.value}/${baseAmount}`;
@@ -29,37 +32,62 @@ export default function App() {
     setBase(newBase);
     setPair(newPair);
   }
+  window.addEventListener('resize', () => {
+    setInnerWidth(window.innerWidth);
+  })
 
   return (
     <div className="container">
-      <header>
-        <h1>Currency Converter</h1>
+      <header id="header">
+        <h1>
+          Currency Converter
+          <FontAwesomeIcon
+            icon={faCommentsDollar}
+            style={{marginLeft: '20px'}}
+          />
+        </h1>
       </header>
 
-      <main>
-        <header>
-          <h3>From</h3>
-        </header>
+      <main id="main">
 
+        <div id="base">
+          <h2 className="label">From</h2>
           <input
+            id="baseAmount"
+            placeholder="Enter an amount"
+            type="number"
             value={baseAmount}
             onChange={e => setBaseAmount(e.target.value)}
-            />
+          />
           <Select
+            className="selection"
             options={currencyCodes}
             value={base}
             onChange={setBase}
+          />
+        </div>
+
+        <div id="btn-container">
+          <button className="btn" onClick={handleReverse}>
+            <FontAwesomeIcon
+              icon={faExchangeAlt}
+              transform={{rotate: width > 720 ? 0 : 90}}
             />
-          <button onClick={handleReverse}>Reverse</button>
-          <h3>To</h3>
-          <p>{conversion}</p>
+          </button>
+        </div>
+
+        <div id="pair">
+          <h2 className="label">To</h2>
+          <p className="amount">{conversion}</p>
           <Select
+            className="selection"
             options={currencyCodes}
             value={pair}
             onChange={setPair}
-            />
+          />
+        </div>
 
-        </main>
+      </main>
     </div>
   );
 }
